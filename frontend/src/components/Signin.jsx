6e875094739d56f4e5cddbf0f33079d7068a8a6d
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-06-23 22:37:05 -0400

    Refactoring the auth code

---
+++ frontend/src/components/Signin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/signin.module.scss";
+
+const Signin = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Signin;
