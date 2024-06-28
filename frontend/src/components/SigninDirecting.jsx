198436d1567a6441e7b699484cc11e52c476a0b5
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-06-28 00:19:46 -0400

    Codes signin routing

--- frontend/src/components/SigninDirecting.jsx
+++ frontend/src/components/SigninDirecting.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "../styles/signinDirecting.module.scss";
+import { useRouter } from "next/navigation";
 
 const SigninDirecting = () => {
+  const router = useRouter();
   return (
     <>
       <div className={styles.container}>
         <h3>Welcome Back</h3>
         <p>Sign in with your creditential to connect with friends</p>
-        <button>Sign In</button>
+        <button type="submit" onClick={() => router.push("/Signin")}>
+          Sign In
+        </button>
       </div>
       {/* Background Code */}
       <div class={styles.background}>
