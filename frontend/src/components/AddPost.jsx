a71989b2b7dd054473e86b29a4e57733b82ee373
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-07-07 02:09:15 -0400

    Setting adding the post successfully

--- frontend/src/components/AddPost.jsx
+++ frontend/src/components/AddPost.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import styles from "../styles/addPost.module.scss";
 import Image from "next/image";
 import { useFormik } from "formik";
-import { useState } from "react";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 const endpoint = "/posts/create_post";
@@ -12,21 +12,25 @@ const AddPost = ({ onRefresh }) => {
   const [error, setError] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isSubmited, setIsSubmited] = useState(false);
+  const imageUploadRef = useRef(null);
+  const contentRef = useRef(null);
+  const [imageName, setImageName] = useState(null);
 
   const initialValues = {
     content: "",
-    // image: null,
+    image: null,
   };
 
   const { values, handleChange, handleSubmit, setFieldValue } = useFormik({
     initialValues: initialValues,
-    onSubmit: async () => {
+    onSubmit: async (e) => {
       setIsLoading(true);
 
       const formData = new FormData();
       for (let key in values) {
         formData.append(key, values[key]);
       }
+      console.log(formData);
       const token = localStorage.getItem("token");
       try {
         const response = await fetch(url, {
@@ -41,14 +45,30 @@ const AddPost = ({ onRefresh }) => {
         }
 
         setIsSubmited(true);
+
         const data = await response.json();
         setIsLoading(false);
         onRefresh();
       } catch (err) {
         throw new Error("Failed to add a post");
       }
+
+      //resetting the form
+      setFieldValue("content", "");
+      setImageName("");
     },
   });
+
+  const handleImageChange = (e) => {
+    setFieldValue("image", e.target.files[0]);
+    console.log("File change event:", e);
+    console.log(e.target.files[0].name);
+    setImageName(e.target.files[0].name);
+  };
+  const handleImageClick = (e) => {
+    e.preventDefault();
+    imageUploadRef.current.click();
+  };
 
   return (
     <div className={styles.container}>
@@ -65,18 +85,27 @@ const AddPost = ({ onRefresh }) => {
         <input
           id="content"
           placeholder="Share your act . . . "
+          ref={contentRef}
           type="text"
           autoComplete="off"
           value={values.content}
           onChange={handleChange}
         />
         <div className={styles.buttons}>
-          <button className={styles.addPhoto}>
+          <button className={styles.addPhoto} onClick={handleImageClick}>
             <a>
               <Image src="/upload.svg" width={20} height={20} alt="Upload" />
               <p>Add a photo </p>
             </a>
           </button>
+          <input
+            type="file"
+            id="image"
+            ref={imageUploadRef}
+            style={{ display: "none" }}
+            onChange={handleImageChange}
+          />
+          <p className={styles.addedImageName}>{imageName}</p>
           <div className={styles.post} type="submit" onClick={handleSubmit}>
             <a>
               <p>{isLoading ? "Posting..." : "Post"}</p>
