33380bc0a760f78414f8deb29201726a1f721d65
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-07-13 11:45:26 -0400

    Adds to the models the relationship between User, Like and other models

--- backend/app/models.py
+++ backend/app/models.py
@@ -16,6 +16,24 @@ class User(Base):
     profile_pic = Column(String, nullable=True)
     created_at = Column(String, default=str(datetime.now()), nullable=False)
 
+    #relationship with created posts
+    posts = relationship('Post', back_populates='owner', cascade="all, delete, save-update") #relationship with user_id in Post
+
+    # liked_posts = relationship('Post', secondary='likes', back_populates='liked_by_users')
+
+    #relationship with likes
+    likes = relationship('Like', back_populates="liking_user", cascade='all, delete, save-update')
+
+    #relationship with comments
+    comments = relationship('Comment', back_populates='owner', cascade='all, delete, save-update')
+
+    #relationship with I did its
+    i_did_its = relationship('I_did_it', back_populates='owner', cascade='all, delete, save-update')
+
+    #relationship with I will do its
+    i_will_do_its = relationship('I_will_do_it', back_populates='owner', cascade='all, delete, save-update')
+
+
 class Post(Base):
     __tablename__ = 'posts'
     
@@ -24,38 +42,78 @@ class Post(Base):
     image = Column(String, nullable=True)
     created_at = Column(String, default=str(datetime.now()), nullable=False)
 
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    owner = relationship('User') #returns the owner attributes of the user who created the post
-    likes = Column(Integer, default=0)
+    #reltionship with post owner
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False) # relationship with User model
+    owner = relationship('User', back_populates= 'posts') #returns the owner attributes of the user who created the post
+
+    #relatinoship with likes
+    likes = relationship('Like', back_populates='post', cascade='all, delete, save-update')
+
+    #relationship with I did its
+    i_did_its = relationship('I_did_it', back_populates='post', cascade='all, delete, save-update')
+
+    #relationship with I will do its
+    i_will_do_its = relationship('I_will_do_it', back_populates='post', cascade='all, delete, save-update')
+
+    #relationship with comments
+    comments = relationship('Comment', back_populates='post', cascade='all, delete, save-update')
+
+    # liked_by_users = relationship('User', secondary='likes', back_populates='liked_posts')
+
+    # likes = Column(Integer, default=0)
 
 class Like(Base):
     __tablename__ = 'likes'
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key =True)
-    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key =True)
     created_at = Column(String, default=str(datetime.now()), nullable=False)
+
+    #relationship with the liking user
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key =True)
+    liking_user = relationship('User', back_populates='likes')
+
+    #relationship with the post
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key =True)
+    post = relationship('Post', back_populates='likes')
+    
 
     # post = relationship('Post', back_populates='likes')
 
 class I_did_it(Base):
     __tablename__ = 'i_did_it'
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key =True)
-    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key =True)
     created_at = Column(String, default=str(datetime.now()), nullable=False)
+
+    #relationship with the owner of the I did it
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key =True)
+    owner = relationship('User', back_populates='I_did_its')
+
+    #relationship with post
+    post = relationship('Post', back_populates='I_did_its')
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key =True)
+
 class I_will_do_it(Base):
     __tablename__ = 'i_will_do_it'
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key =True)
-    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key =True)
     created_at = Column(String, default=str(datetime.now()), nullable=False)
+
+    #relationship with the owner of the I will do it
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key =True)
+    owner = relationship('User', back_populates='i_will_do_its')
+
+    #relationship with post
+    post = relationship('Post', back_populates='i_will_do_its')
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key =True)
 
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key = True, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
     content = Column(String, nullable=False)
     created_at = Column(String, default=str(datetime.now()), nullable=False)
+
+    #relationship with comment owner
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE', onupdate='CASCADE'))
+    owner = relationship('User', back_populates='comments')
     
-    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'))
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key =True)
+    #relationship with post
+    post = relationship('Post', back_populates='comments')
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE', onupdate='CASCADE'))
 
     
 
--- backend/app/routers/post.py
+++ backend/app/routers/post.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException, status, APIRouter, UploadFile, File, Form
+from fastapi import Depends, FastAPI, HTTPException, status, APIRouter, UploadFile, File, Form, Response
 from typing import Optional
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -29,20 +29,27 @@ async def read_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).options(joinedload(models.Post.owner)).order_by(models.Post.id.desc()).all()
     return posts
 
-@router.post('/{id}', response_model = schemas.PostResponse)
+@router.post('/{id}')
 async def like_post(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = 'Post not found')
     
     # Update likes count in the Post table
-    post.likes += 1
+    like_row = db.query(models.Like).filter(models.Like.post_id == id, models.Like.user_id == current_user.id).first()
+    if like_row:
+        db.delete(like_row)
+        post.likes -= 1
+        db.commit()
+        db.refresh(post)
+        return {"message": "Post is unliked", "likes": post.likes}
 
+    post.likes += 1
     # Create a new Like record
     like = models.Like(user_id = current_user.id, post_id = id)
     db.add(like)
-    
     db.commit()
     db.refresh(post)
-    return post
+    print(post)
+    return {"message": "Post is liked", "likes": post.likes}
     
