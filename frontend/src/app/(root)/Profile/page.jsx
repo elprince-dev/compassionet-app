dfe99924555fdbd76bb3233261d7b954967756de
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-07-22 23:01:53 -0400

    Finishes the header section of profile

--- frontend/src/app/(root)/Profile/page.jsx
+++ frontend/src/app/(root)/Profile/page.jsx
@@ -3,9 +3,13 @@ import React, { useContext } from "react";
 import styles from "./page.module.scss";
 import UserContext from "@/constants/userContext";
 import Image from "next/image";
+import { FaRegCommentDots } from "react-icons/fa";
+import { LuListTodo } from "react-icons/lu";
+import { IoMdDoneAll } from "react-icons/io";
 
 const page = () => {
   const { user } = useContext(UserContext);
+  console.log(user);
   return (
     <div className={styles.profile}>
       <div className={styles.profileContainer}>
@@ -19,9 +23,36 @@ const page = () => {
                 alt="Picture of the user"
               />
             </div>
-            <div className={styles.user}></div>
+            <div className={styles.user}>
+              <h2>
+                {user.first_name} {user.last_name}
+              </h2>
+              <p>@{user.username}</p>
+            </div>
           </div>
-          <div className={styles.bio}></div>
+          <div className={styles.bio}>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              sint nam. Quae, similique odio culpa magni cumque aliquid
+              voluptatum quidem perspiciatis exercitationem impedit tenetur
+              assumenda nesciunt distinctio fugiat natus adipisci?{user.bio}
+            </p>
+          </div>
+        </div>
+        <hr className={styles.line} />
+        <div className={styles.icons}>
+          <div className={styles.icon}>
+            <FaRegCommentDots />
+            <h4>My Acts</h4>
+          </div>
+          <div className={styles.icon}>
+            <LuListTodo />
+            <h4>To Do</h4>
+          </div>
+          <div className={styles.icon}>
+            <IoMdDoneAll />
+            <h4>Done</h4>
+          </div>
         </div>
       </div>
     </div>
